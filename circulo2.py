2b521c8a89e35904b438f68267788cef3eee628a
IMW_11ARIAN <127745976+ArianRamos@users.noreply.github.com>
2023-11-28 23:39:50 +0000

    Scipt @

---
+++ circulo2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class circulo():
+
+    def __init__(self,centro,radio):
+        self.centro=centro
+        self.radio=radio
+
+    def mostrar(self):
+        return "Centro:{0}-Radio:{1}".format(self.centro.mostrar(),self.radio)
